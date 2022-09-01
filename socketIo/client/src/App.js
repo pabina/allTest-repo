95be2fb664a00d988fbfd4570226e2cd76b13f74
pabina <rimalpabina1234@gmail.com>
2022-09-01 16:49:39 +0545

    join room

--- socketIo/client/src/App.js
+++ socketIo/client/src/App.js
@@ -5,10 +5,19 @@ import { useEffect,useState } from 'react';
 const socket=io.connect("http://localhost:8006");
  
 const App = () => {
+  const [room,setRoom]=useState("");
   const [message,setMessage]=useState("");
   const [messageReceived,setMessageReceive]=useState("")
+  
+  
+  const joinRoom=()=>{
+    if(room !== ""){
+      socket.emit("join_room",room);
+    }
+  }
+  
   const sendMessage=()=>{
-    socket.emit("send_message",{message});
+    socket.emit("send_message",{message,room});
   };  
 
 
@@ -25,9 +34,16 @@ const App = () => {
   return (
   <>
  <div className="App">
+    <input placeholder='Room number' onChange={(event)=>{setRoom(event.target.value)}}/>
+    <button onClick={joinRoom}>joinRoom</button>
+
+   <br></br>
+
   <input type="text" placeholder='message' onChange={(event)=>{
     setMessage(event.target.value)
   } }/>
+
+
   <button onClick={sendMessage}> send message</button>
  <h1>message:</h1>
  {messageReceived}
