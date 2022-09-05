bd846f744f266df6e7f387df95eae81e8a28bca8
pabina <rimalpabina1234@gmail.com>
2022-09-05 22:32:53 +0545

    user models added

---
+++ E-commerce/api/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import Connection from "./db/DBConnection.js";
+import userRoute from "./routes/user_route.js";
+
+const app=express();
+dotenv.config()
+
+//connected to database
+Connection();
+
+//adding middleware
+app.use(express.json());
+
+//calling differents routes
+app.use("/api/user",userRoute);
+
+
+app.get("/api/test",()=>{
+    console.log("test is successfull");
+})
+
+
+
+
+
+
+
+//listening to server
+app.listen(process.env.PORT,()=>{
+    console.log("server is running");
+})
